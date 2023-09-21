d4c47b28f1ee50a534cc291d44f89c07c31e6fae
Lucas Arenas Arteaga <lucario_9@hotmail.com>
2023-09-20 20:47:23 -0500

    Get roles

---
+++ KometSales.Api/Controllers/UserRolesController.cs
@@ -0,0 +1,38 @@
+﻿using KometSales.Application.UserRoles.Queries;
+using KometSales.Common.Entities.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KometSales.Api.Controllers
+{
+    /// <summary>
+    /// User roles controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Administrator")]
+    public class UserRolesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserRolesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets roles list
+        /// </summary>
+        /// <returns>List of roles</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<UserRolDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<List<UserRolDto>> Get()
+        {
+            return await _mediator.Send(new GetUserRolesQueryHandler.Query());
+        }
+    }
+}
---
+++ KometSales.Application/UserRoles/Queries/GetUserRolesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using KometSales.Common.Entities.Dtos;
+using KometSales.Ifrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace KometSales.Application.UserRoles.Queries
+{
+    public class GetUserRolesQueryHandler
+    {
+        public class Query : IRequest<List<UserRolDto>>
+        {
+        }
+
+        public class Handler : IRequestHandler<Query, List<UserRolDto>>
+        {
+            private readonly ServiceDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(ServiceDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<List<UserRolDto>> Handle(
+                Query query,
+                CancellationToken cancellationToken
+            )
+            {
+                return _mapper.Map<List<UserRolDto>>(await _context.UserRoles.ToListAsync());
+            }
+        }
+    }
+}
